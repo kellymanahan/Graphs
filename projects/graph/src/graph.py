6bc88db47d445da8c1050b8a7459e9f6e9c62ec2
Alexander <alexander.botello@g.austincc.edu>
2018-08-28 20:58:37 -0500

    Write out Graph class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,29 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
-
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+
+    def add_edge(self, start, end):
+        if start in self.vertices and end in self.vertices:
+            self.vertices[start].add(end)
+            return
+        raise ValueError
+
+
+if __name__ == "__main__":
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_vertex('4')
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '3')
+    graph.add_edge('4', '1')
+    print(graph.vertices)
