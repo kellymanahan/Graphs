8d748497a1a8081f6d62cf013aacfaf91ff5b206
kammagik <kammagik@yahoo.com>
2018-08-30 21:09:36 -0700

    i need more practice

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,33 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+class Vertex:
+    def __init__(self, label):
+        self.label = label
+        self.edges = set()
 
-
+    def __repr__(self):
+        return 'Vertex' + self.label
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+       self.vertices = {}
+    
+    def add_vertex(self, vertex):
+        if vertex in self.vertices:
+            raise ValueError('there is already a vertex')
+        else:
+            self.vertices[vertex] = set()
+
+    def add_edge(self, start, end):
+        if start not in self.vertices or end not in self.vertices:
+            return False
+        else:
+            self.vertices[start].add(end)
+            return True
+
+
+    def bfs(graph, start):
+        nodes = []
+        
+    
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,11 +5,72 @@ Demonstration of Graph and BokehGraph functionality.
 """
 
 from sys import argv
+from graph import Graph
+import random
+
+#solution code
+def draw_random_graph(graph, vertices, edges):
+    for i in range(vertices):
+        graph.add_vertex(str(i))
+        j= 0
+        while j <= edges: 
+            success = graph.add_edge(str(random.randint(0, vertices-1)),str(random.randint(0, vertices-1)))
+        if success:
+            j += 1
+    return graph
+
+def print_graph(graph):
+    for v in graph.vertices:
+        line = ""
+        line += (str(v) + ": " )
+        for e in graph.vertices[v]:
+            line += str( e ) + " "
+        print( line )
+#solution code
+def bfs(graph, start_vert):
+    nodes = [] 
+    colors = {}
+    my_que = collections.deque()
+    for key in graph.vertices.keys():
+        colors[key] = "white"
+
+    my_queue.qppend(str(start_vert))
+    colors[start_vert] = "grey"
+
+    while len(my_queue) != 0:
+        node = my_queue[0]
+        neighbors = graph.vertices[node]
+        for n in neighbors:
+            if colors[n] =="white":
+                my_queue.append(str(n))
+                colors[n] = "grey"
+        colors[node] = "black"
+        nodes.append (my_queue.popleft())
+
+        return nodes
+        
+def dfs(graph):
+    nodes = [] 
+    colors = {}
+    my_que = collections.deque()
+    for key in graph.vertices.keys():
+        colors[key] = "white"
+    def DFS_visit(v):
+        colors[v] = "grey"
+        nodes.append(v)
+        for n in graph.vertices[str(v)]:
+            if colors[n] =="white"
+            nodes.append(n)
+            DFS_visit(n)
+        colors[v] = "black"
+
+    return nodes
 
 
 def main():
-    pass  # TODO
-
+    graph = Graph()
+    graph = draw_random_graph(graph, 5, 10)
+    print_graph(graph)
 
 if __name__ == '__main__':
     # TODO - parse argv
